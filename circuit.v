bea1bcb3a4932ae4ee551d0b480a2465b2ff2097
PEATeixeira <peat.am096@gmail.com>
2020-11-17 22:56:03 +0000

    Resolucao exercico 3

--- circuit.v
+++ circuit.v
@@ -2,16 +2,32 @@
 `include "iob_lib.vh"
 
 module circuit
+  #(
+    parameter W=32
+    )
   (
    `INPUT(rst,1),
    `INPUT(clk,1),
    `INPUT(en,1),
-   `INPUT(x,32),
-   `OUTPUT(y,32)
+   `INPUT(x,W),
+   `OUTPUT(y,W)
    );
 
-   `SIGNAL(acc, 32)
-   `ACC_ARE(clk, rst, 1'b0, en, acc, x)
-   `SIGNAL2OUT(y, acc)
+   `SIGNAL(cnt, 7) //7-bit counter as iterator
+   //`SIGNAL(x, W) //x sinal aleatorio (dá erro)
+   `SIGNAL(y_int, W) //internal y
+   `SIGNAL(y_int_n_1, W) //y(n-1)
+
+   `SIGNAL2OUT(y, y_int) //connect internal y to output
+
+   //y shift register 
+   `REG_ARE(clk, rst, 1'b0, cnt!=99, y_int_n_1, y_int) 
+
+   //iteration counter
+   `COUNTER_ARE(clk, rst, cnt!=99, cnt)
+
+   //compute function
+   `COMB y_int = 2 * y_int_n_1 + x;
+   
    
 endmodule
--- circuit_tb.v
+++ circuit_tb.v
@@ -1,13 +1,15 @@
 `timescale 1ns / 1ps
 `include "iob_lib.vh"
 
+`define W 96
+//`define A 32 dá erro (dá erro)
 module circuit_tb;
 
    `CLOCK(clk, 10)
    `RESET(rst, 1, 10)
    `SIGNAL(en, 1)
    `SIGNAL(x, 32)
-   `SIGNAL_OUT(y, 32)
+   `SIGNAL_OUT(y, `W)
 
    integer i;
    
@@ -16,15 +18,20 @@ module circuit_tb;
       $dumpvars();
       en=0;
       for (i=0; i<100; i=i+1) begin
-         @(posedge clk) #1 x=i; en=1;
-         @(posedge clk) #1 en=0;
+         @(posedge clk) #1 en=1 ; 
+         @(posedge clk) #1 x=$random; $display("%d: %d",i+1, y, x);//criar valores aleatorios para x
+          
       end
 
       @(posedge clk) #100 $finish;
 
    end 
 
-   circuit c0
+   circuit
+     #(
+       .W(`W)
+       ) 
+   c0
      (
       .rst(rst),
       .clk(clk),
